3ba225bdd480760123bc501c60024c313c19cea7
katusha <ekaterina.reynol@gmail.com>
2020-03-02 23:05:53 +0300

    Tried to fix problem with running jenkins test.

--- src/test/java/Fixtures/BrowserFixture.java
+++ src/test/java/Fixtures/BrowserFixture.java
@@ -5,18 +5,13 @@ import PageObjects.CartPage;
 import PageObjects.HeadPage;
 import PageObjects.UserPage;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URI;
 
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
@@ -40,16 +35,7 @@ public class BrowserFixture {
         capabilities.setCapability("enableVideo", false);
         capabilities.setAcceptInsecureCerts(false);
 
-        WebDriver driver = null;
-        try {
-            driver = new RemoteWebDriver(
-                    URI.create("http://selenoid:4444/wd/hub").toURL(),
-                    capabilities
-            );
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        WebDriverRunner.setWebDriver(driver);
+        WebDriverManager.chromedriver().setup();
 
         Configuration.browserCapabilities = capabilities;
 
