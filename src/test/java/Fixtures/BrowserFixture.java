92ff3f931e2c0dc154a414ce925584a8df2f57fc
katusha <ekaterina.reynol@gmail.com>
2020-03-03 21:33:09 +0300

    Fixed problem with running jenkins test.

--- src/test/java/Fixtures/BrowserFixture.java
+++ src/test/java/Fixtures/BrowserFixture.java
@@ -5,17 +5,13 @@ import PageObjects.CartPage;
 import PageObjects.HeadPage;
 import PageObjects.UserPage;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
@@ -25,24 +21,23 @@ import static com.codeborne.selenide.Selenide.open;
 public class BrowserFixture {
     protected String correctEmail = "test@mail.ru";
     protected String correctPassword = "test";
-    protected String userPageUrl = "http://automationpractice.com/index.php?controller=my-account";
+    static protected String userPageUrl = "http://automationpractice.com/index.php?controller=my-account";
     protected HeadPage headPage = new HeadPage();
     protected UserPage userPage = new UserPage();
     protected CartPage cartPage = new CartPage();
 
-    @BeforeEach
-    public void beforeTest() {
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.baseUrl = userPageUrl;
+    @BeforeAll
+    public static void beforeTest() {
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1980x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "http://automationpractice.com";
 
-        open(userPageUrl);
+        Selenide.open("/");
     }
 
     @AfterEach
